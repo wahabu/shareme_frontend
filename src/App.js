0ad5761e43399e213a15869962dea6a24e9c67da
wahabu <ritscg2hibo@gmail.com>
2023-04-22 00:11:56 +0900

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // To create a basic functional component type: "rafce" this will be create structure of a react functional component
 import React from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
+
 const App = () => {
   return (
     <Routes>

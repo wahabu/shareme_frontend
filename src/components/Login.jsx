0c97b25d808e65eecd5208528b54bb6d85993122
wahabu <ritscg2hibo@gmail.com>
2023-04-30 23:35:24 +0900

    Update Login.jsx & App.js

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
 
 const Login = () => {
   return (

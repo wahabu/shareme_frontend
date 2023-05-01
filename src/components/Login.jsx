6312a25df2ba07add195bb79a0033c95e0a4351b
wahabu <ritscg2hibo@gmail.com>
2023-05-01 22:29:05 +0900

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,13 +2,53 @@ import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from 'react-icons/fc';
-import shareVideo from '../assets/share.mp4';
+import shareVideo from '../assets/share202305.mp4';
 import logo from '../assets/logowhite.png';
 
 const Login = () => {
+
+  const responseGoogle= (response) => {
+    
+  }
+
   return (
-    <div>
-      Login
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video 
+          src={shareVideo}
+          type="video/mp4"
+          Loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+
+          <div className="shadow-2x1">
+            <GoogleLogin 
+              clientId=''
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="m4-4" /> Sign in with Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
